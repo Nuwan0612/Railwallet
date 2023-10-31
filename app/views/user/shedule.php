f181c19f40118ebd4f9eed80d43134b52622976c
Nuwan0612 <nuwanfernando17@gmail.com>
2023-10-31 19:03:38 +0530

    version 2

--- app/views/user/shedule.php
+++ app/views/user/shedule.php
@@ -1,40 +1,4 @@
-<?php require APPROOT . '/views/user-db/header.php';?>
-<div class="side-menu">
-        <div class="brand-name">
-            <h3>User-K.V.Dias</h3>
-        </div>
-        <ul>
-            <a href="user.html"><li> <img src="<?php echo URLROOT;?>public/pics/wallet.png" alt="">&nbsp;<span>TopUp</span></li></a>
-            <a href="#"><li><img src="<?php echo URLROOT;?>public/pics/seats.png" alt="">&nbsp;<span>Seat Reservation</span></li></a>
-            <a href="#"><li><img src="<?php echo URLROOT;?>public/pics/ticket.png" alt="">&nbsp;<span>Ticket Prices</span></li></a>
-            <a href="#"><li><img src="<?php echo URLROOT;?>public/pics/money-transfer.png" alt="">&nbsp;<span>Transaction History</span></li></a>
-            <a href="u_schedule.html"><li><img src="<?php echo URLROOT;?>public/pics/september.png" alt="">&nbsp;<span>Shedule</span></li></a>
-            <a href="#"><li><img src="<?php echo URLROOT;?>public/pics/feedback.png" alt="">&nbsp;<span>Feedback</span></li></a>
-            <!--<a href="#"> <li><img src="help-web-button.png" alt="">&nbsp; <span>Help</span></li> -->
-            <a href="#"><li><img src="<?php echo URLROOT;?>public/pics/customer-service.png" alt="">&nbsp;<span>Customer Support</span></li></a>
-            <a href="#"><li><img src="<?php echo URLROOT;?>public/pics/settings.png" alt="">&nbsp;<span>Settings</span></li></a>
-        </ul>
-    </div>
-    <div class="container">
-        <div class="header">
-            <div class="nav">
-
-                <a href="#"><h3> QR Scanner</h3></a>
-                <a href="#"><h3> View Ticket</h3></a>
-                <!-- <a href="#"><h3> Shedule</h3></a> -->
-                <a href="#"><h3>Fine Details </h3></a>
-                <a href="#"><h3> Log Out</h3></a>
-
-                <!-- <a href="#"><h3> Fine Details</h3></a> -->
-                <div class="user">
-                    
-                <img src="<?php echo URLROOT;?>public/pics/notification.png" alt="">
-                    <div class="img-case">
-                        <img src="man.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
+<?php require APPROOT . '/views/user/header.php';?>
         <div class="content">
             <div class="cards">
                 <div class="card">
@@ -154,4 +118,4 @@
             </div>
         </div>
     </div>
-    <?php require APPROOT . '/views/user-db/footer.php';?>   
+    <?php require APPROOT . '/views/user/footer.php';?>   
