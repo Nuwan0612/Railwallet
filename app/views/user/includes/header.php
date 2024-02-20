0ecc6640291ece02f29c55098232fabcccdf80e2
Viranga Dias <2021cs050@stu.ucsc.cmb.ac.lk>
2024-02-20 13:11:48 +0530

    chanege user shedule

--- app/views/user/includes/header.php
+++ app/views/user/includes/header.php
@@ -5,12 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/passenger/user.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/passenger/rating.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/passenger/ratingView.css">
     <!-- <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/user-db/booking.css"> -->
     <!-- <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/user-db/shedule_list.css"> -->
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/passenger/shedule.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/passenger/search_shedule.css">
 
     <title><?php echo SITENAME; ?></title>
 </head>
--- app/views/user/shedule.php
+++ app/views/user/shedule.php
@@ -1,48 +1,75 @@
 <?php require APPROOT . '/views/user/includes/header.php';?>
-<div class="container-main">
-        <div class="container2">
-            <div class="container3">
-            
-    <div class="wrapperBooking">
-        <div class="select-btn">
-            <span class="Select Station">Departure Station</span>
-            <i class="uil uil-angle-down"> </i>
+<div class="deatails">
+    <div class="all-trains">
+
+      <div class="head">
+        <div class="title2">Train Schedules</div>
+        <!-- <a href="<?php echo URLROOT; ?>admins/addTrainShedule"><button class="add-train">Add Shedule</button></a> -->
+      </div>
+
+      <div class="search-bar-outer-container-shedule">
+
+      <div class="wrapper_shedule">
+      <div class="search-bar-inner-container-shedule1">      
+          <div class="search-bar-shedule">
+          <input type="text" id="search-departure-station" placeholder="Departure Station">
+            <button class="search-button" onclick="searchDepartureStation()">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+          <div class="result-box1">
+            <!-- <ul>
+                <li>Javascript</li>
+                <li>Python</li>
+            </ul> -->
+          </div>
+
         </div>
-        <div class="content1">
-            <div class="search">
-                <i class="uil uil-search"></i>
-                <input type="text" placeholder="search" id="searchInput1">
+
+        <div class="search-bar-inner-container-shedule2">
+          <div class="search-bar-shedule">
+          <input type="text" id="search-arrival-station" placeholder="Arrival Station">
+            <button class="search-button" onclick="#">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
+          <div class="result-box2">
+            <!-- <ul>
+                <li>Javascript</li>
+                <li>Python</li>
+            </ul> -->
             </div>
-            <ul class="options" id="options1">
-            </ul>
         </div>
-    </div>
 
-    <div class="wrapper2">
-        <div class="select-btn2">
-            <span class="Select Station">Arrival Station</span>
-            <i class="uil uil-angle-down"> </i>
+        <div class="search-bar-inner-container-shedule3">
+          <div class="search-bar-shedule">
+          <input type="text" id="search-date" placeholder="Date" onfocus="(this.type = 'date')" onblur="(this.type='text')">
+            <button class="search-button" onclick="#">
+              <i class="fas fa-search"></i>
+            </button>
+          </div>
         </div>
-        <div class="content2">
-            <div class="search2">
-                <i class="uil uil-search"></i>
-                <input type="text" placeholder="search" id="searchInput2">
+ 
+        <div class="hide-outer-container">
+        <a class="links" href="<?php echo URLROOT;?>admins/deactivatedShedules"><button class="search-btn">Search</button></a>
+        </div>        
+      </div>
+      </div>
+
+      <div class="detail-body">
+        <div class="card-box">
+            <div class="card1">
+                <div class="train_details">
+                <h3><span class="train-name"> Gaalu Kumari</span></h3>
+                    <span class="train-type">Express</span> 
+                </div>
+
+                <div class="departure-status">
+                    <h4>Hikkaduwa</h4>
+                </div>
             </div>
-            <ul class="options2" id="options2">
-            </ul>
         </div>
+      </div>  
     </div>
-    </div>
-    <div class="date">
-        <input type="date">
-    </div>
-   
-<div class="text">
-<input type="button" value="Search" id="btn">
-</div>
-</div>
-
-
-</div>
-</div>
+  </div>
     <?php require APPROOT . '/views/user/includes/footer.php';?>   
