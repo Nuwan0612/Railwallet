50b239c05170e4df2783384f4c648f2943634ab3
Viranga Dias <2021cs050@stu.ucsc.cmb.ac.lk>
2024-04-14 10:05:37 +0530

    a

--- app/config/config.php
+++ app/config/config.php
@@ -3,7 +3,7 @@
   //DB Params
   define('DB_HOST', 'localhost');
   define('DB_USER', 'root');
-  define('DB_PASSWORD', '');
+  define('DB_PASSWORD', '12345678');
   define('DB_NAME', 'railwallet');
 
   // App Root
