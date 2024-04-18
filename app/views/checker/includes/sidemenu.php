7af49565b07bbff7899e1cfbc8687297774f43c2
KeshaliDhananjana <keshdhananjana@gmail.com>
2024-04-18 10:22:28 +0530

    checker interface 2

--- app/views/checker/includes/sidemenu.php
+++ app/views/checker/includes/sidemenu.php
@@ -8,14 +8,14 @@
     </div>
 
     <ul>
-        <a href="<?php echo URLROOT; ?>checkers/dashboard"><li><img src="<?php echo URLROOT;?>public/pics/db.png" >&nbsp;<span>Dashboard</span></li></a>
-        <a href="<?php echo URLROOT; ?>/checkers/users"><li><img src="<?php echo URLROOT;?>public/pics/man.png">&nbsp;<span>Users</span></li></a>
+        <a href="<?php echo URLROOT; ?>/checkers/qrScan"><li><i class='bx bx-qr-scan' ></i><span>QR Scanner</span></li></a>
+        <a href="<?php echo URLROOT; ?>/checkers/users"><li><i class='bx bx-money' ></i><span>Fines</span></li></a>
         <!-- <a href="#"><li><img src="customer-service.png" alt="">&nbsp;<span>Checkers</span></li></a>
         <a href="#"><li><img src="feedback.png" alt="">&nbsp;<span>FeedBacks</span></li></a> -->
-        <a href="<?php echo URLROOT; ?>/checkers/shedules"><li><img src="<?php echo URLROOT;?>public/pics/september.png" >&nbsp;<span>Shedule</span></li></a>
-        <a href="#"> <li><img src="<?php echo URLROOT;?>public/pics/customer-service.png" >&nbsp; <span>Customer Support</span></li>
-        <a href="#"><li><img src="<?php echo URLROOT;?>public/pics/settings.png" >&nbsp;<span>Settings</span></li></a>
-        <a href="<?php echo URLROOT; ?>/users/logout"><li><img src="<?php echo URLROOT;?>public/pics/shutdown.png" >&nbsp;<span>LogOut</span></li></a>
+        <a href="<?php echo URLROOT; ?>/checkers/shedules"><li><i class='bx bxs-calendar' ></i><span>Shedule</span></li></a>
+        <a href="#"> <li><i class='bx bx-support' ></i><span>Customer Support</span></li>
+        <!-- <a href="#"><li><i class='bx bxs-cog' ></i><span>Settings</span></li></a> -->
+        <a href="<?php echo URLROOT; ?>/users/logout"><li><i class='bx bx-log-out-circle' ></i><span>LogOut</span></li></a>
 
     </ul>
 </div>
@@ -23,16 +23,19 @@
 <div class="checker-container">
     <div class="checker-header">
         <div class="checker-nav">
-            <a href="<?php echo URLROOT; ?>checkers/qrScan"><h3>QR Scanner</h3></a>
+            <!-- <a href="<?php echo URLROOT; ?>checkers/qrScan"><h3>QR Scanner</h3></a>
             <a href="#"><h3> Add Fines</h3></a>
             <a href="#"><h3> Issue Ticket</h3></a>
-            <a href="#"><h3> Cancle Ticket</h3></a>
+            <a href="#"><h3> Cancle Ticket</h3></a> -->
             <!-- <a href="#"><h3> Log Out</h3></a> -->
             <div class="user">
                 
-            <img src="<?php echo URLROOT;?>public/pics/notification.png" >
+            <!-- <img src="<?php echo URLROOT;?>public/pics/notification.png" > -->
+            <p>Welcome Checker</p>
+            <img src="<?php echo URLROOT;?>public/pics/man.png" >
             <div class="img-case">
-                <img src="<?php echo URLROOT;?>public/pics/man.png" >
+                <!-- <img src="<?php echo URLROOT;?>public/pics/man.png" > -->
+                <i class='bx bxs-bell-ring' ></i>
             </div>
         </div>
     </div>
