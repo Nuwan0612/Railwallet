a063828a5a69baa72e3faabf12b792cf0bc9277f
KeshaliDhananjana <keshdhananjana@gmail.com>
2024-04-19 09:43:04 +0530

    Add payhere sandbox

--- app/views/user/transaction.php
+++ app/views/user/transaction.php
@@ -1,31 +1,57 @@
 <?php require APPROOT . '/views/user/includes/header.php';?>
 <div class="transaction-container">
-    <form action="">
+    <form action="https://sandbox.payhere.lk/pay/checkout" method="POST" id="payhere-payment-form">
+        <h3 class="transaction-title">Payment Details:</h3>
         <div class="row">
+        <!-- <h3 class="transaction-title">Payment Details</h3> -->
             <div class="col">
-                <h3 class="transaction-title">User Details</h3>
+                <!-- <h3 class="transaction-title">User Details</h3> -->
                 <div class="inputBox">
                     <span>full name :</span>
                     <input type="text" placeholder="Kavindu Perera">
                 </div>
-                <div class="inputBox">
+                <!-- <div class="inputBox">
                     <span>email :</span>
                     <input type="email" placeholder="kavindu@gmail.com">
-                </div>
+                </div> -->
                 <div class="inputBox">
                     <span>phone number :</span>
 
                     <input type="tel" placeholder="+94 713 456 289">
                 </div>
-                <div class="inputBox">
+                <!-- <div class="inputBox">
                     <span>cards accepted :</span>
-                    <!-- <img src="card_img.png" alt=""> -->
+                    <img src="card_img.png" alt="">
                     <img src="<?php echo URLROOT;?>public/css/index/card_img.png" class="ticket-logo">
 
-                </div>
+                </div> -->
             </div>
             <div class="col">
-                <h3 class="transaction-title">Card Details</h3>
+                <div class="inputBox">
+                    <span>email :</span>
+                    <input type="email" placeholder="kavindu@gmail.com">
+                </div>
+                <div class="inputBox">
+                    <span>Amount :</span>
+                    <input type="text" placeholder="1000.00">
+                </div>
+                <input type="hidden" name="merchant_id" value="1226507"> 
+                <input type="hidden" name="return_url" value="http://localhost/payment/Success.php">
+                <input type="hidden" name="cancel_url" value="/Fail.php">
+                <input type="hidden" name="notify_url" value="http://localhost:8888/payhere">
+                <input type="hidden" name="first_name" value="Keshali">
+                <input type="hidden" name="last_name" value="Dhananjana">
+                <input type="hidden" name="email" value="keshali@gmail.com">
+                <input type="hidden" name="phone" value="0717737663">
+                <input type="hidden" name="address" value="Colombo">
+                <input type="hidden" name="city" value="Galle">
+                <input type="hidden" name="country" value="Sri Lanka">
+                <input type="hidden" name="order_id" value="123">
+                <input type="hidden" name="items" value="123">
+                <input type="hidden" name="currency" value="LKR">
+                <input type="hidden" name="amount" value="1000.00">
+                <input type="hidden" name="hash" id="hashField">
+                <!-- <h3 class="transaction-title">Card Details</h3>
                 <div class="inputBox">
                     <span>name on card :</span>
                     <input type="text" placeholder="Mr. Kavindu Perera">
@@ -52,11 +78,11 @@
                         <span>exp month :</span>
                         <input type="text" placeholder="january">
                     </div>
-                </div>
+                </div> -->
             </div>
         </div>
 
-        <input type="submit" value="Pay here" class="submit-btn">
+        <button type="submit" onclick="calculateChecksum()">Pay Here</button>
     </form>
 </div>
 <?php require APPROOT . '/views/user/includes/footer.php';?>
