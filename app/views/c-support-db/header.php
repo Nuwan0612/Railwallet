d81e50f81ae611bb6239445efcdb07429fcce929
vini2811 <savini.ur@gmail.com>
2024-04-21 23:50:15 +0530

    custom-supporter editing

--- app/views/c-support-db/header.php
+++ app/views/c-support-db/header.php
@@ -11,16 +11,19 @@
 
 <div class="side-menu">
         <div class="brand-name">
-            <h3><?php echo SITENAME; ?></h3>
+        <div class="logo">
+            <img src="<?php echo URLROOT;?>public/css/index/logo1.png" class="logo" width="55" height="55">
+        </div>
+            <p><?php echo SITENAME; ?></p>
         </div>
         <ul>
-            <a href="<?php echo URLROOT?>supporters/dashboard"><li><img src="<?php echo URLROOT;?>public/pics/db.png" >&nbsp;<span>Dashboard</span></li></a>
-            <a href="<?php echo URLROOT?>supporters/users"><li><img src="<?php echo URLROOT;?>public/pics/man.png">&nbsp;<span>Users</span></li></a>
+            <a href="<?php echo URLROOT?>supporters/dashboard"><li><img src="<?php echo URLROOT;?>img/dashboard.png" >&nbsp;<span>Dashboard</span></li></a>
+            <a href="<?php echo URLROOT?>supporters/users"><li><img src="<?php echo URLROOT;?>img/user.png">&nbsp;<span>Users</span></li></a>
             <!-- <a href="checker.html"><li><img src="<?php echo URLROOT;?>public/pics/qr-code.png" >&nbsp;<span>Checkers</span></li></a> -->
-            <a href="<?php echo URLROOT?>supporters/feedbacks"><li><img src="<?php echo URLROOT;?>public/pics/feedback.png" >&nbsp;<span>FeedBacks</span></li></a>
-            <a href="<?php echo URLROOT?>supporters/shedules"><li><img src="<?php echo URLROOT;?>public/pics/september.png" >&nbsp;<span>Shedule</span></li></a>
+            <a href="<?php echo URLROOT?>supporters/feedbacks"><li><img src="<?php echo URLROOT;?>img/feedback.png" >&nbsp;<span>FeedBacks</span></li></a>
+            <a href="<?php echo URLROOT?>supporters/shedules"><li><img src="<?php echo URLROOT;?>img/calendar.png" >&nbsp;<span>Shedule</span></li></a>
             <!--<a href="#"> <li><img src="help-web-button.png" alt="">&nbsp; <span>Help</span></li> -->
-            <a href="#"><li><img src="<?php echo URLROOT;?>public/pics/settings.png" >&nbsp;<span>Settings</span></li></a>
+            <a href="#"><li><img src="<?php echo URLROOT;?>img/settings.png" >&nbsp;<span>Settings</span></li></a>
         </ul>
     </div>
     <div class="container">
